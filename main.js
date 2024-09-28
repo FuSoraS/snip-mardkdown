99ff8516019ba58e712dc6553ce46dfc891a4092
FuSoraS <developer.FuSoraS@gmail.com>
2024-09-28 18:38:36 -0300

    refactor: add title

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 let button = document.getElementById("buttonSubmit");
 
 button.addEventListener("click", () => {
+    const titulo = document.getElementById("titulo").value;
     let plataforma = document.getElementById("plataforma").value;
     let link = document.getElementById("link").value;
     let tiempoInicio = document.getElementById("tiempoInicio").value;
     let tiempoFin = document.getElementById("tiempoFin").value;
     let nota = document.getElementById("nota").value;
     let codeMd = document.getElementById("codeMd");
-    codeMd.innerHTML = `### [${tiempoInicio} - ${tiempoFin}] titulos
+    codeMd.innerHTML = `### [${tiempoInicio} - ${tiempoFin}] ${[titulo]}
 [🎧 Play ${plataforma} - 2min️ (${tiempoInicio} - ${tiempoFin})](${link})
 #### Note
 ${nota}`
